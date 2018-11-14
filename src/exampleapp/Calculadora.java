8fbc07439d0edb0d88529e533d7d05dc592aa196
jordiBujaldon <jordibujal@gmail.com>
2018-11-14 16:30:20 +0100

    Commit inicial

---
+++ src/exampleapp/Calculadora.java
@@ -0,0 +1,7 @@
+package exampleapp;
+
+public class Calculadora {
+    public int addTwoIntegers(int a, int b) {
+        return a + b;
+    }
+}
---
+++ test/exampleapp/spec/ConfigTest.java
@@ -0,0 +1,13 @@
+package exampleapp.spec;
+
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+import exampleapp.Calculadora;
+
+@RunWith(ConcordionRunner.class)
+public class ConfigTest {
+    public int getTestResult(int a, int b) {
+        Calculadora calc = new Calculadora();
+        return (calc.addTwoIntegers(a, b));
+    }
+}
